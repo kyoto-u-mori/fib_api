b9236ddae63bdcfb3f86f66fe18424075c52a6e6
kyoto-u-mori <mori.kazuhito.63r@st.kyoto-u.ac.jp>
2023-07-31 02:38:00 +0900

    デプロイ用のフォルダを追加

---
+++ fib-api-project/fibonacci.js
@@ -0,0 +1,29 @@
+// fibonacci.js
+
+function fibonacci(n) {
+    if ((typeof n) !== 'number' || n <= 0) {
+      throw new Error('Invalid input. n must be a positive integer.');
+    }
+    //初期値の処理
+    else if (n === 1) {
+      return 1n;
+    }
+    else if (n === 2) {
+      return 1n;
+    }
+    else {
+        let prev = 1n;
+        let current = 1n;
+      
+        for (let i = 3; i <= n; i++) {
+          const next = prev + current;
+          prev = current;
+          current = next;
+        }
+        return current;
+    }
+
+  }
+  
+  module.exports = fibonacci;
+  
---
+++ fib-api-project/index.js
@@ -0,0 +1,43 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+const fibonacci = require('./fibonacci');
+
+const convertStringToNumber = n => {
+  if( typeof n !== "string" || n === "" ) return false;
+  const num =  Number( n ); // 数値に変換できない場合はNaNが返る
+  return isNaN( num ) ? false : num;
+}
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get('/fib', (req, res) => {
+  const n = convertStringToNumber(req.query.n);
+  try {
+    const fib_result = fibonacci(n);
+    resValue = fib_result.toString(); //桁数が溢れた時のため文字列に変換(要検討)
+    res.json({'result':resValue});
+  } catch (error) {
+    res.status(400).json({ 'status': 400, 'message': 'Bad request.' });
+  }
+});
+
+//テンプレート
+// app.get("/path", (req, res, next) => {
+//   return res.status(200).json({
+//     message: "Hello from path!",
+//   });
+// });
+
+// app.use((req, res, next) => {
+//   return res.status(404).json({
+//     error: "Not Found",
+//   });
+// });
+
+module.exports.handler = serverless(app);
